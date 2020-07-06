90c8ccc045e8fcef5fcd3513541fb198e7a9c56e
Ilya Lezhepecov <770-175@mail.ru>
2020-07-06 14:48:56 +0300

    edited index.html file and edited views.py file

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
+from django.template import loader
 from django.http import HttpResponse
 from .models import Question
 
 def index(request):
     lastest_question_list= Question.objects.order_by('-pub_date')[:5]
-    output=', '.join([q.question_text for q in lastest_question_list])
-    return HttpResponse(output)
+    template = loader.get_template('polls/index.html')
+    context = {
+        'lastest_question_list':lastest_question_list
+    }
+    return HttpResponse(template.render(context, request))
 
-# def index(request):
-#     return HttpResponse("hello world you're at the polls index")
 
 def detail(request, question_id):
     return HttpResponse("You're ooking at question %s." %question_id)\
