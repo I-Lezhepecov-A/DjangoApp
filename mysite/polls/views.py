743e56392383bc80937faceeb6270fc9e3e2805d
Ilya Lezhepecov <770-175@mail.ru>
2020-07-06 14:24:15 +0300

    edited index function

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
 
 def index(request):
-    return HttpResponse("hello world you're at the polls index")
+    lastest_question_list= Question.objects.order_by('-pub_date')[:5]
+    output=', '.join([q.question_tezt for q in lastest_question_list])
+    return HttpResponse(output)
+
+# def index(request):
+#     return HttpResponse("hello world you're at the polls index")
 
 def detail(request, question_id):
     return HttpResponse("You're ooking at question %s." %question_id)\
@@ -13,4 +19,3 @@ def results(request, question_id):
 
 def vote(request, question_id):
     return HttpResponse ("You're voting on question %s." %question_id)
-    
